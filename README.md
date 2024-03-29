85bcc1ec5703416aaade4735b4a03168cb61f3ae
Brienna Ransom <cometgrrl@gmail.com>
2024-03-28 21:28:26 -0700

    added BlogPost class.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # post_creator
-I recently converted my blog to a Eleventy blog using Netlify. Now that's done, I wanted to automate publishing new posts.
+I recently converted [my blog](https://puppydogkisses.com) to a Eleventy blog using Netlify. Now that's done, I wanted to automate publishing new posts.
 
 - Netlify will automatically build the site anytime changes are commited to the [repository](https://github.com/cometgrrl/puppydogkisses).
 - Each post consists of an image, tags, a title, and a post date. 
@@ -13,6 +13,6 @@ Here's how it works:
 -  The image filenames are in the format YYYYMMDD__tags__title.jpeg
     - post_creator uses the date as the post date, then fills in the tags, title, and image parameters in the markdown.
 - Once the markdown files are created, they are moved to the blog's local repository and committed. 
-- Netlify will then automatically rebuild the site and voila, new puppy photos are published on [Puppy Dog Kisses](https://puppydogkisses.com)
+- Netlify will then automatically rebuild the site and voila, new puppy photos are published on [Puppy Dog Kisses](https://puppydogkisses.com).
 
-I didn't do a lot of error handling, that's all for a future version!
+I didn't do a lot of error handling, that's all for a future version! I did, however, update it to use OOP.
--- post_creator.py
+++ post_creator.py
@@ -19,147 +19,156 @@ test_mode = False
 REPOSITORY_PATH = "/Users/brienna/Code/puppydogkisses/content/blog/"
 IMAGES_FOLDER = "/Users/brienna/Code/puppydogkisses_images_for_posts/"
 
-# this function creates a markdown file for a post based on the image filename
-def create_markdown_file(image_filename):
-    #check if the post is ready to be published
-    if image_filename[:8] <= datetime.datetime.now().strftime('%Y%m%d'):
-       print(f"Image is ready for posting: {image_filename}")
-    else:
-        print(f"Image skipped: {image_filename}")
+class BlogPost:
+    def __init__(self, image_file_name):
+        self.image_file = image_file_name
+        self.filename = image_file_name.split(".")[0] # save the filename without the extension
+        self.markdown_file = f"{self.filename}.md"
+        self.markdown_image_path = f"/img/{self.image_file}" # set the image path based on the filename
+        self.title_string = ""
+        self.tag_string = ""
+        self.image_file_path = f"{IMAGES_FOLDER}{self.image_file}"
+        self.destination_path = f"{REPOSITORY_PATH}{self.image_file}"
+        self.markdown_file_path = f"{IMAGES_FOLDER}{self.markdown_file}"
+        self.markdown_destination_path = f"{REPOSITORY_PATH}{self.markdown_file}"
+
+    # this function creates a markdown file for a post based on the image filename
+    def create_markdown_file(self):
+        #check if the post is ready to be published
+        if self.filename[:8] <= datetime.datetime.now().strftime('%Y%m%d'):
+            print(f"Image is ready for posting: {self.filename}")
+        else:
+            print(f"Image skipped: {self.filename}")
+            return 1
+        
+        # split the filename into parts so we can get the title, date, and tags
+        self.split_filename = self.filename.split("__")
+        
+        # get the date from the filename
+        self.date = datetime.datetime.strptime(self.split_filename[0], '%Y%m%d')
+        self.date = self.date.replace(hour=20, minute=0, second=0, microsecond=0)
+       
+       #get the title from the filename
+        title_list = self.split_filename[2].split("_") # add some error handing here
+        for word in title_list:
+            self.title_string += word
+            self.title_string += " "
+
+        # get the tags from the filename
+        tags = self.split_filename[1].split("_") # add some error handing here
+        for tag in tags:
+            self.tag_string += tag
+        self.tag_string.strip()
+        if self.tag_string[-1:] == ",":
+            self.tag_string = self.tag_string[:-1] # remove the last comma if it exists
+
+        # create the markdown file in the images folder
+        with open(f"{IMAGES_FOLDER}{self.markdown_file}", "w") as file:
+            file.write(f"---\n")
+            file.write(f"image: \"{self.markdown_image_path}\"\n")
+            file.write(f"title: {self.title_string}\n")
+            file.write(f"date: {self.date}\n")
+            file.write(f"tags: {self.tag_string}\n")
+            file.write(f"---\n")
+            file.write(f"{{% image './{self.image_file}', '' %}}")
+        print (f"{self.markdown_file} created")
         return 0
-    # set the image path based on the filename
-    md_image_path = f"/img/{image_filename}"
-    
-    # remove the file extension from the title and split the filename into parts so we can get the title, date, and tags
-    filename_without_extension = image_filename.split(".")[0]
-    split_filename = filename_without_extension.split("__")
-    
-    title = split_filename[2].split("_")
-    title_string = ""
-    for word in title:
-        title_string += word
-        title_string += " "
 
-    tags = split_filename[1].split("_")
-
-    tag_string = ""
-
-    for tag in tags:
-        tag_string += tag
-    
-    tag_string.strip()
-   
-    if tag_string[-1:] == ",":
-        tag_string = tag_string[:-1] # remove the last comma
-
-    date = datetime.datetime.strptime(split_filename[0], '%Y%m%d')
-    date = date.replace(hour=8, minute=0, second=0, microsecond=0)
-
-    # create the markdown file in the images folder
-    markdown_filename = f"{filename_without_extension}.md"
-    with open(f"{IMAGES_FOLDER}{markdown_filename}", "w") as file:
-        file.write(f"---\n")
-        file.write(f"image: \"{md_image_path}\"\n")
-        file.write(f"title: {title_string}\n")
-        file.write(f"date: {date}\n")
-        file.write(f"tags: {tag_string}\n")
-        file.write(f"---\n")
-        file.write(f"{{% image './{image_filename}', '' %}}")
-    print (f"{markdown_filename} created")
-    return {"image_filename": image_filename, "markdown_filename": markdown_filename}
-
-# this function moves the images and markdown files to the correct folder
-def move_files(post):        
-    image_file_path = f"{IMAGES_FOLDER}{post['image_filename']}"
-    destination_path = f"{REPOSITORY_PATH}{post['image_filename']}"
-    md_file_path = f"{IMAGES_FOLDER}{post['markdown_filename']}"
-    md_destination_path = f"{REPOSITORY_PATH}{post['markdown_filename']}"
-    os.rename(image_file_path, destination_path)
-    os.rename(md_file_path, md_destination_path)
-    print(f"Files moved to repository: {post['image_filename']}, {post['markdown_filename']}")
-
-    return post
+    # this function moves the images and markdown files to the correct folder
+    def move_files(self):        
+        os.rename(self.image_file_path, self.destination_path) # add some error handing here
+        os.rename(self.markdown_file_path, self.markdown_destination_path) # add some error handing here
+        print(f"Files moved to repository: {self.filename}, {self.markdown_file}")
+        return 0
 
 
-# this function publishes the posts to the blog by adding, committing, and pushing the files to the repository
-def publish_posts(file_to_publish):
-    today = datetime.date.today()
-    git_add_command ="git add "
-    git_commit_command = f"git commit -m  'New posts for {today}'"
-    git_push_command = "git push -u origin HEAD"
+    # this function publishes the posts to the blog by adding, committing, and pushing the files to the repository
+    def publish_posts(self):
+        today = datetime.date.today()
+        git_add_command ="git add "
+        git_commit_command = f"git commit -m  'New posts for {today}'"
+        git_push_command = "git push -u origin HEAD"
 
-    # add filenames to the git add command
-    git_add_command += f"{file_to_publish['image_filename']} {file_to_publish['markdown_filename']} "
-    # run the git add command
+        # add filenames to the git add command
+        git_add_command += f"{self.image_file} {self.markdown_file} "
+        # run the git add command
 
-    os.chdir(REPOSITORY_PATH)
-    
-    add_result = subprocess.run(git_add_command, shell=True, capture_output=True, text=True)
-    if add_result.returncode == 0:
-        print("Add command executed successfully.")
-        print("Output:", add_result.stdout)        
-        print(f"Files were successfully added.")
-        # run the git commit command
-        commit_result = subprocess.run(git_commit_command, shell=True, capture_output=True, text=True)
-        if commit_result.returncode == 0:
-            print("Commit command executed successfully.")  
-            print("Output:", commit_result.stdout)
-            print(f"Files were successfully committed.")
-            # run the git push command
-            push_result = subprocess.run(git_push_command, shell=True, capture_output=True, text=True)
-            if push_result.returncode == 0:
-                print("Push command executed successfully.")
-                print("Output:", push_result.stdout)
-                print(f"Files were successfully pushed.")
+        os.chdir(REPOSITORY_PATH)
+        
+        add_result = subprocess.run(git_add_command, shell=True, capture_output=True, text=True)
+        if add_result.returncode == 0:
+            print("Add command executed successfully.")
+            print("Output:", add_result.stdout)        
+            print(f"Files were successfully added.")
+            # run the git commit command
+            commit_result = subprocess.run(git_commit_command, shell=True, capture_output=True, text=True)
+            if commit_result.returncode == 0:
+                print("Commit command executed successfully.")  
+                print("Output:", commit_result.stdout)
+                print(f"Files were successfully committed.")
+                # run the git push command
+                push_result = subprocess.run(git_push_command, shell=True, capture_output=True, text=True)
+                if push_result.returncode == 0:
+                    print("Push command executed successfully.")
+                    print("Output:", push_result.stdout)
+                    print(f"Files were successfully pushed.")
+                else:
+                    print("Push command execution failed.")
+                    print("Error:", push_result.stderr) 
+                    print(f"Files were not pushed.")
+                    exit()
             else:
-                print("Push command execution failed.")
-                print("Error:", push_result.stderr) 
-                print(f"Files were not pushed.")
+                print("Commit command execution failed.")
+                print("Error:", commit_result.stderr)
+                print(f"Files were not committed.")
                 exit()
         else:
-            print("Commit command execution failed.")
-            print("Error:", commit_result.stderr)
-            print(f"Files were not committed.")
+            print("Add command execution failed.")
+            print("Error:", add_result.stderr) 
+            print(f"Files were not added.")
             exit()
-    else:
-        print("Add command execution failed.")
-        print("Error:", add_result.stderr) 
-        print(f"Files were not added.")
-        exit()
-    return 1
+        return 0
 
 #images_folder_path = "/Users/brienna/Documents/PuppyDogKisses"  # replaced with IMAGES_PATH
 files = os.listdir(IMAGES_FOLDER)
-created_count = 0  # Initialize post_count variable
-post_files = []
-
+post_list = []
+created_count = 0
+result = -1
 # Check if there are files in the folder, if so, create a post for each image (.jpeg) file
 if files:
     for file in files:
         if file.endswith(".jpeg"):
-            create_result = create_markdown_file(file) # Create markdown file based on the image filename
-            if create_result != 0:
-                post_files.append(create_result)  # Append the result to the list
+            blog_post = BlogPost(file)
+            result = blog_post.create_markdown_file() # Create markdown file 
+            if result == 0:
+                post_list.append(blog_post)  # Append the result to the list
                 created_count += 1 # Increment the post count
-else:
-    print("The folder is empty.")
-if created_count == 1:
+elif created_count == 1:
     print(f"{created_count} post successfully created.")
 elif created_count > 1:
     print(f"{created_count} posts successfully created.")
 else: print("No posts were created.")
 
-files_to_publish = []  # Initialize files_to_publish list
+posts_to_publish = []  # Initialize files_to_publish list
 published_count = 0 # Initialize published_count variable
+move_result = -1
+publish_result = -1
 
 if test_mode is False: # check for test mode
-    for post in post_files: # move the files to the repository location, if not in test mode
-        move_result = move_files(post)
-        files_to_publish.append(move_result)
-    for file in files_to_publish: # publish the files to the repository location, if not in test mode
-        published_count += publish_posts(file) # Publish the posts
-        print(f"Post added to files_to_publish: {post}")
+    for post in post_list: # move the files to the repository location, if not in test mode
+        move_result = BlogPost.move_files(post)
+        if move_result == 0:
+            posts_to_publish.append(post)  # Append the post to the list to be published
+            print(f"Post added to posts_to_publish: {post.image_file}")
+    for post in posts_to_publish: # publish the files to the repository location, if not in test mode
+        publish_result = BlogPost.publish_posts(post) # Publish the posts
+        if publish_result == 0:
+            published_count +=1
+            print(f"Post published: {post.image_file}")
 else:  print("Test mode: files not moved or commited to repository.")
+if published_count == 1:
+    print("1 post published.")
+else: print(f"{published_count} posts published.")
 
 
 
